d7f21b0a94da9c6933f4261d952a78120de5cd16
IsadinovDias312 <112857129+IsadinovDias312@users.noreply.github.com>
2023-04-13 23:44:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
+chat_id = 1182463770
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    tmp = ((x_success+y_success) * y_cnt)/(x_cnt+y_cnt)    
+    sqr = (y_success - tmp)**2/tmp
+    if sqr < 3.5:
+      return False
+    else:
+      return True
